517c3d0d1db0c3c794b9e94ac30bd2e96b3d3930
Cyrus Glanzer <cyrusglanzer@gmail.com>
2021-08-23 16:27:24 -0400

    Create simple circular spirograph pattern with randomized colors

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
-from turtle import Turtle, Screen
+import turtle as t
+import random
 
-turtle = Turtle()
+ANGLE_CHANGE = 3
+t.colormode(255)
 
-for i in range(4):
-    turtle.forward(100)
-    turtle.right(90)
+turtle = t.Turtle()
+turtle.width(3)
+turtle.speed("fastest")
 
-screen = Screen()
+
+def rand_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_color = (r, g, b)
+    return random_color
+
+
+for i in range(int(360/ANGLE_CHANGE)):
+    turtle.color(rand_color())
+    turtle.circle(100)
+    current_direction = turtle.heading()
+    turtle.setheading(current_direction+ANGLE_CHANGE)
+
+screen = t.Screen()
 screen.exitonclick()
