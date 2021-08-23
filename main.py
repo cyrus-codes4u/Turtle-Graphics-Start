9f7b68ad687a21ceca7d5997eb30d3b6f1670450
Cyrus Glanzer <cyrusglanzer@gmail.com>
2021-08-23 15:37:40 -0400

    Made color choices actually random

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from turtle import Turtle, Screen
+import turtle as t
 import random
 
-turtle = Turtle()
-turtle.width(5)
+t.colormode(255)
+turtle = t.Turtle()
+turtle.width(15)
 turtle.speed("fastest")
-colors = ["DarkOrchid", "wheat", "SeaGreen", "SlateGray", "DeepSkyBlue", "IndianRed"]
 directions = [0, 90, 180, 270]
+
 
 def draw_shape(num_sides):
     angle = 360 / num_sides
@@ -14,11 +15,19 @@ def draw_shape(num_sides):
         turtle.right(angle)
 
 
+def rand_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_color = (r, g, b)
+    return random_color
+
+
 for i in range(1000):
-    turtle.color(random.choice(colors))
+    turtle.color(rand_color())
     turtle.setheading(random.choice(directions))
     turtle.forward(10)
 
 
-screen = Screen()
+screen = t.Screen()
 screen.exitonclick()
