8d4bb296ab51dcd1bf5ed3a933177f7ae753480a
Cyrus Glanzer <cyrusglanzer@gmail.com>
2021-08-23 14:45:35 -0400

    Draws shapes in sequence with increasing number of sides

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ from turtle import Turtle, Screen
 
 turtle = Turtle()
 
-for i in range(10):
-    turtle.forward(10)
-    if turtle.isdown():
-        turtle.up()
-    else:
-        turtle.down()
+
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    for j in range(num_sides):
+        turtle.forward(100)
+        turtle.right(angle)
+
+
+for i in range(3, 11):
+    draw_shape(i)
+
 
 screen = Screen()
 screen.exitonclick()
